19d4ac8d305f13791694e18c896669896a82c2df
HenriqueFutema <minifut0407@gmail.com>
2019-09-09 19:29:15 -0300

    Menu v-if getIsLogged

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -5,8 +5,9 @@
         <router-link id="link" to="/">Vaquinha</router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text to="/signin">Entrar</v-btn>
-      <v-btn light to="/signup">Criar Conta</v-btn>
+      <v-btn text to="/signin" v-if="!getIsLogged">Entrar</v-btn>
+      <v-btn light to="/signup" v-if="!getIsLogged">Criar Conta</v-btn>
+      <v-btn light to="/signup" v-if="getIsLogged">Sair</v-btn>
       <v-btn text to="/">Contato</v-btn>
     </v-app-bar>
   </div>
@@ -18,3 +19,13 @@
   color: white;
 }
 </style>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(["getIsLogged"])
+  }
+};
+</script>
