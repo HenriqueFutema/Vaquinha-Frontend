785cfbf83bcebdae4aad3a4593fa0b1396ecf388
HenriqueFutema <minifut0407@gmail.com>
2019-09-11 18:01:41 -0300

    View Perfil e PerfilComponent

---
+++ src/views/Perfil.vue
@@ -0,0 +1,16 @@
+<template>
+  <Perfil />
+</template>
+
+<script>
+import Perfil from "@/components/PerfilComponent.vue";
+
+export default {
+  components: {
+    Perfil
+  }
+};
+</script>
+
+<style>
+</style>
