0538d5fa84271ffd9e33ae7e2af0692e7a87670e
HenriqueFutema <minifut0407@gmail.com>
2019-09-12 20:57:27 -0300

    PerfilComponent getIsLogged e redirect

--- src/views/Perfil.vue
+++ src/views/Perfil.vue
@@ -3,12 +3,26 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import Perfil from "@/components/PerfilComponent.vue";
 
 export default {
   components: {
     Perfil
+  },
+
+  computed:{
+    ...mapGetters(['getIsLogged'])
+  },
+
+  created: function(){
+    if (!this.getIsLogged) {
+      this.$router.push('/signin')
+    }
   }
+
+  
+
 };
 </script>
 
