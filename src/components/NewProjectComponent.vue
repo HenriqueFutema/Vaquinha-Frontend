5b6386f5f676327651234391cd4fd6718527d122
HenriqueFutema <minifut0407@gmail.com>
2019-09-15 17:51:01 -0300

    NewProject form layout

--- src/components/NewProjectComponent.vue
+++ src/components/NewProjectComponent.vue
@@ -1,5 +1,39 @@
 <template>
-  <h1>Novo Projeto</h1>
+  <v-container>
+    <v-layout>
+      <v-flex xs12 md12 class="mx-1">
+        <v-card class="px-5 py-3">
+          <h1>Novo Projeto</h1>
+          <v-card-title>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field
+                  label="Nome do Projeto"
+                  outlined
+                  required
+                  color="#F3B61F"
+                  rounded
+                  autofocus
+                ></v-text-field>
+              </v-col>
+
+              <v-col cols="12">
+                <v-textarea outlined name="input-7-4" label="Descrição" value color="#F3B61F"></v-textarea>
+              </v-col>
+
+              <v-col cols="12">
+                <v-text-field label="HashTags" outlined required color="#F3B61F" rounded autofocus></v-text-field>
+              </v-col>
+
+              <v-col cols="12">
+                <v-btn elevation="10" color="success">Criar</v-btn>
+              </v-col>
+            </v-row>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -18,5 +52,9 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+/* .cont {
+  height: 100%;
+  width: 100%;
+} */
 </style>
